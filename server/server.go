9703a37e5257923cc85f5ffb30c0b950197323f1
piglig <zhu1197437384@gmail.com>
2023-06-20 10:18:13 +0800

    feat: graceful shutdown

--- server/server.go
+++ server/server.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"context"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	"net/http"
+	"os"
+	"os/signal"
 	"pwebsocket/route"
+	"syscall"
 )
 
 type Server struct {
@@ -19,7 +24,23 @@ func New(addr string) *Server {
 }
 
 func (s *Server) Listen() {
+	s.Echo.Logger.SetLevel(log.DEBUG)
+
 	route.InitRoute(s.Echo)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		sig := <-sigs
+		s.Logger.Info("received signal", sig.String())
+		defer cancel()
+		err := s.Echo.Shutdown(ctx)
+		if err != nil {
+			s.Logger.Fatal(err)
+			return
+		}
+	}()
 
 	err := s.Echo.Start(s.addr)
 	if err != nil {
